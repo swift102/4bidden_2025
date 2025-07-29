205c13ea41e1ea9903e6e86ece9695dc5f44251d
Lokusa Thwala <96729155+Lokusa@users.noreply.github.com>
2025-07-29 21:42:51 +0200

    AlertController

---
+++ Backend/Controllers/AlertsController.cs
@@ -0,0 +1,37 @@
+﻿
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+//using Backend.Data;
+using Backend.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AlertsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AlertsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAlerts()
+    {
+        var alerts = _context.Alerts.OrderByDescending(a => a.Timestamp).ToList();
+        return Ok(alerts);
+    }
+
+    [HttpPost]
+    public IActionResult CreateAlert(Alert alert)
+    {
+        var southAfricaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("South Africa Standard Time");
+        alert.Timestamp = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, southAfricaTimeZone);
+
+        _context.Alerts.Add(alert);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(GetAlerts), new { id = alert.Id }, alert);
+    }
+
+}
