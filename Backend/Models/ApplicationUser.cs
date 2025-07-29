69b716fb14c9d1f13dad831da368931381b491e7
Portia Mosokotso <102986782+21638684@users.noreply.github.com>
2025-07-29 19:53:26 +0200

    Login and Auth controller added

--- Backend/Models/ApplicationUser.cs
+++ Backend/Models/ApplicationUser.cs
@@ -7,10 +7,7 @@ namespace Backend.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-        public bool EmailNotifications { get; set; } = true;
-        public string NotificationThreshold { get; set; } = "Medium";
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public string Password { get; set; } = string.Empty;
+
     }
 }
---
+++ Backend/Models/LogIn.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Models
+{
+    public class LogIn
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
